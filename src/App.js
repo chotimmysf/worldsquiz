9941a6802e5cce5bdd97c5c2525e82c5a70c21c4
Timmy Cho <chotimmysf@gmail.com>
2022-11-10 21:35:06 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>World's Randomest Quiz</h1>
+        <p>Take the world's randomest quiz!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/questionBank.js
@@ -0,0 +1,332 @@
+export const questionBank = [
+    // Question 1
+    {
+        questionText: "What is the world's most spoken language?",
+        answerChoices: [
+            {answerText: "Spanish", isCorrect: false},
+            {answerText: "Mandarin Chinese", isCorrect: false},
+            {answerText: "Swahili", isCorrect: false},
+            {answerText: "English", isCorrect: true}
+        ]
+    },
+    // Question 2
+    {
+        questionText: `Who is best known for the song "All I want for Christmas Is You"?`,
+        answerChoices: [
+            {answerText: "Mariah Carey", isCorrect: true},
+            {answerText: "Ariana Grande", isCorrect: false},
+            {answerText: "Taylor Swift", isCorrect: false},
+            {answerText: "Marilyn Monroe", isCorrect: false}  
+        ]
+    },
+    // Question 3
+    {
+        questionText: "What cuisine are tacos associated with?",
+        answerChoices: [
+            {answerText: "Canadian", isCorrect: false},
+            {answerText: "Californian", isCorrect: false},
+            {answerText: "Mexican", isCorrect: true},
+            {answerText: "Middle Eastern", isCorrect: false}
+        ]
+    },
+    // Question 4
+    {
+        questionText: "Which country are you NOT allowed to be naked in the window of your own home?",
+        answerChoices: [
+            {answerText: "Singapore", isCorrect: true},
+            {answerText: "Japan", isCorrect: false},
+            {answerText: "France", isCorrect: false},
+            {answerText: "China", isCorrect: false}  
+        ]
+    },
+    // Question 5
+    {
+        questionText: "What is the most popular mobile app?",
+        answerChoices: [
+            {answerText: "Facebook", isCorrect: false},
+            {answerText: "TikTok", isCorrect: true},
+            {answerText: "Snapchat", isCorrect: false},
+            {answerText: "Roblox", isCorrect: false}  
+        ]
+    },
+    // Question 6
+    {
+        questionText: "Which is the most known video game character?",
+        answerChoices: [
+            {answerText: "Pikachu", isCorrect: false},
+            {answerText: "Mario", isCorrect: false},
+            {answerText: "Pac-Man", isCorrect: true},
+            {answerText: "Naruto", isCorrect: false}  
+        ]
+    },
+    // Question 7
+    {
+        questionText: "Which TV show has so much positive feedback, it was considered by the Internet to be a worthy piece of Japanese animation?",
+        answerChoices: [
+            {answerText: "Pokemon", isCorrect: false},
+            {answerText: "Death Note", isCorrect: false},
+            {answerText: "Cory in the House", isCorrect: true},
+            {answerText: "Spirited Away", isCorrect: false}  
+        ]
+    },
+    // Question 8
+    {
+        questionText: "Which of the following sports teams are the most globally famous?",
+        answerChoices: [
+            {answerText: "New York Yankees", isCorrect: false},
+            {answerText: "Los Angeles Lakers", isCorrect: false},
+            {answerText: "San Francisco Giants", isCorrect: false},
+            {answerText: "Manchester United", isCorrect: true}  
+        ]
+    },
+    // Question 9
+    {
+        questionText: "What is 1 plus 1?",
+        answerChoices: [
+            {answerText: "0", isCorrect: false},
+            {answerText: "1", isCorrect: false},
+            {answerText: "2", isCorrect: true},
+            {answerText: "3", isCorrect: false}  
+        ]
+    },
+    // Question 10
+    {
+        questionText: "Which company sells the iPhone?",
+        answerChoices: [
+            {answerText: "Microsoft", isCorrect: false},
+            {answerText: "Apple", isCorrect: true},
+            {answerText: "Google", isCorrect: false},
+            {answerText: "Amazon", isCorrect: false}  
+        ]
+    },
+    // Question 11
+    {
+        questionText: "How many Harry Potter books are in the series?",
+        answerChoices: [
+            {answerText: "16", isCorrect: false},
+            {answerText: "34", isCorrect: false},
+            {answerText: "1", isCorrect: false},
+            {answerText: "7", isCorrect: true}  
+        ]
+    },
+    // Question 12
+    {
+        questionText: "Who is the most famous chef?",
+        answerChoices: [
+            {answerText: "Gordon Ramsay", isCorrect: true},
+            {answerText: "Gordon Ramsey", isCorrect: false},
+            {answerText: "Wolfgang Puck", isCorrect: false},
+            {answerText: "Woofgang Puck", isCorrect: false}  
+        ]
+    },
+    // Question 13
+    {
+        questionText: "What fashion brand is the most hated for its practices?",
+        answerChoices: [
+            {answerText: "Forever 21", isCorrect: false},
+            {answerText: "Shein", isCorrect: true},
+            {answerText: "Dolls Kill", isCorrect: false},
+            {answerText: "Fashion Nova", isCorrect: false}  
+        ]
+    },
+    // Question 14
+    {
+        questionText: "Which country has the most censorship?",
+        answerChoices: [
+            {answerText: "North Korea", isCorrect: false},
+            {answerText: "China", isCorrect: false},
+            {answerText: "Saudi Arabia", isCorrect: false},
+            {answerText: "Eritrea", isCorrect: true}  
+        ]
+    },
+    // Question 15
+    {
+        questionText: "Which of the following is the 3rd highest grossing mobile game of all time?",
+        answerChoices: [
+            {answerText: "Genshin Impact", isCorrect: false},
+            {answerText: "Honor of Kings", isCorrect: false},
+            {answerText: "Puzzle & Dragons", isCorrect: true},
+            {answerText: "Candy Crush Saga", isCorrect: false}  
+        ]
+    },
+    // Question 16
+    {
+        questionText: "What's the most popular hashtag on Instagram?",
+        answerChoices: [
+            {answerText: "#ootd", isCorrect: false},
+            {answerText: "#love", isCorrect: true},
+            {answerText: "#fashion", isCorrect: false},
+            {answerText: "#beautiful", isCorrect: false}  
+        ]
+    },
+    // Question 17
+    {
+        questionText: "In what year was the King James version of the Bible first published?",
+        answerChoices: [
+            {answerText: "2013", isCorrect: false},
+            {answerText: "1812", isCorrect: false},
+            {answerText: "1611", isCorrect: true},
+            {answerText: "1410", isCorrect: false}  
+        ]
+    },
+    // Question 18
+    {
+        questionText: "In Japan, what is a shinkansen?",
+        answerChoices: [
+            {answerText: "Cargo Train", isCorrect: false},
+            {answerText: "Light Rail", isCorrect: false},
+            {answerText: "Streetcar", isCorrect: false},
+            {answerText: "Bullet Train", isCorrect: true}  
+        ]
+    },
+    // Question 19
+    {
+        questionText: "General Franco was the dictator of which country from 1939 to 1975?",
+        answerChoices: [
+            {answerText: "Portugal", isCorrect: false},
+            {answerText: "Spain", isCorrect: true},
+            {answerText: "France", isCorrect: false},
+            {answerText: "Mexico", isCorrect: false}  
+        ]
+    },
+    // Question 20
+    {
+        questionText: "Paris Baguette originated from _______.",
+        answerChoices: [
+            {answerText: "France", isCorrect: false},
+            {answerText: "United States", isCorrect: false},
+            {answerText: "Japan", isCorrect: false},
+            {answerText: "South Korea", isCorrect: true}  
+        ]
+    },
+    // Question 21
+    {
+        questionText: "Which vegetable is originally purple in colour",
+        answerChoices: [
+            {answerText: "Carrot", isCorrect: true},
+            {answerText: "Eggplant", isCorrect: false},
+            {answerText: "Red Cabbage", isCorrect: false},
+            {answerText: "Asparagus", isCorrect: false}  
+        ]
+    },
+    // Question 22
+    {
+        questionText: "A corncrake is another name for a ______.",
+        answerChoices: [
+            {answerText: "Chicken", isCorrect: false},
+            {answerText: "Turkey", isCorrect: false},
+            {answerText: "Bird", isCorrect: true},
+            {answerText: "Lizard", isCorrect: false}  
+        ]
+    },
+    // Question 23
+    {
+        questionText: "Highland Games are a cultural tradition of which country?",
+        answerChoices: [
+            {answerText: "England", isCorrect: false},
+            {answerText: "Scotland", isCorrect: true},
+            {answerText: "Wales", isCorrect: false},
+            {answerText: "Ireland", isCorrect: false}  
+        ]
+    },
+    // Question 24
+    {
+        questionText: "Har gow, a traditional dim sum food, originated from which province in China?",
+        answerChoices: [
+            {answerText: "Shandong", isCorrect: false},
+            {answerText: "Guangdong", isCorrect: true},
+            {answerText: "Fujian", isCorrect: false},
+            {answerText: "Jiangsu", isCorrect: false}  
+        ]
+    },
+    // Question 25
+    {
+        questionText: "On the farm, what is a kid?",
+        answerChoices: [
+            {answerText: "A baby sheep", isCorrect: false},
+            {answerText: "A baby cow", isCorrect: false},
+            {answerText: "A baby goat", isCorrect: true},
+            {answerText: "A baby pig", isCorrect: false}  
+        ]
+    },
+    // Question 26
+    {
+        questionText: "Imagine you are trying to infect and kill everyone in the world. Which country is the best for doing so before officals take necessary actions to thwart your mission?",
+        answerChoices: [
+            {answerText: "India", isCorrect: false},
+            {answerText: "China", isCorrect: false},
+            {answerText: "Greenland", isCorrect: false},
+            {answerText: "Saudi Arabia", isCorrect: true}  
+        ]
+    },
+    // Question 27
+    {
+        questionText: "What is 1 divided by 1?",
+        answerChoices: [
+            {answerText: "0", isCorrect: false},
+            {answerText: "1", isCorrect: true},
+            {answerText: "2", isCorrect: false},
+            {answerText: "3", isCorrect: false}  
+        ]
+    },
+    // Question 28
+    {
+        questionText: `What company was originally called "Cadabra"?`,
+        answerChoices: [
+            {answerText: "Amazon", isCorrect: true},
+            {answerText: "Adobe", isCorrect: false},
+            {answerText: "Safeway", isCorrect: false},
+            {answerText: "Figma", isCorrect: false}  
+        ]
+    },
+    // Question 29
+    {
+        questionText: "What is 0 minus 1?",
+        answerChoices: [
+            {answerText: "0", isCorrect: false},
+            {answerText: "1", isCorrect: false},
+            {answerText: "-1", isCorrect: true},
+            {answerText: "3", isCorrect: false}  
+        ]
+    },
+    // Question 30
+    {
+        questionText: "In which country can you find The Little Mermaid statue?",
+        answerChoices: [
+            {answerText: "United States", isCorrect: false},
+            {answerText: "Norway", isCorrect: false},
+            {answerText: "Germany", isCorrect: false},
+            {answerText: "Denmark", isCorrect: true}  
+        ]
+    },
+    // Question 31
+    {
+        questionText: "What's the most popular non-local cuisine in Dubai?",
+        answerChoices: [
+            {answerText: "Italian", isCorrect: false},
+            {answerText: "Indian", isCorrect: true},
+            {answerText: "American", isCorrect: false},
+            {answerText: "Chinese", isCorrect: false}  
+        ]
+    },
+    // Question 32
+    {
+        questionText: "In what year did Amazon buy Whole Foods?",
+        answerChoices: [
+            {answerText: "2018", isCorrect: false},
+            {answerText: "2017", isCorrect: true},
+            {answerText: "2012", isCorrect: false},
+            {answerText: "2009", isCorrect: false}  
+        ]
+    },
+    // Question 33
+    {
+        questionText: "In what year did Amazon buy Whole Foods?",
+        answerChoices: [
+            {answerText: "2018", isCorrect: false},
+            {answerText: "2017", isCorrect: false},
+            {answerText: "2012", isCorrect: false},
+            {answerText: "2009", isCorrect: false}  
+        ]
+    },
+]

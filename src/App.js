f4261ec5490baef4ed23c3f289bdb73f41f9ab4d
Timmy Cho <chotimmysf@gmail.com>
2022-11-22 16:11:30 -0800

    questions display without styling

--- src/App.js
+++ src/App.js
@@ -1,14 +1,53 @@
+import React from "react";
 import './App.css';
+import { questionBank } from "./questionBank";
 
-function App() {
+export default function App() {
+  const [ currentQuestion, setCurrentQuestion ] = React.useState(0);
+  const [ score, setScore ] = React.useState(0);
+  const [ showScore, setShowScore ] = React.useState(false);
+
+  const correctAnswerHandler = (isCorrect) => {
+    if(isCorrect) {
+      setScore(score+1);
+    }
+  }
+
+  const nextQuestion = currentQuestion + 1;
+  if (nextQuestion < questionBank.length) {
+    setCurrentQuestion(nextQuestion);
+  } else {
+    setShowScore(true);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="Quiz-header">
         <h1>World's Randomest Quiz</h1>
         <p>Take the world's randomest quiz!</p>
       </header>
+      <div class="quiz-container">
+        {showScore ? (
+          <section className="show-score-section">
+            You got {score} out of {questionBank.length} questions correct!
+          </section>
+        ):(
+          <>
+            <section className='question-section'>
+                <h2>Question {currentQuestion + 1}/{questionBank.length}</h2>
+                <p>{questionBank[currentQuestion].questionText}</p>
+            </section>
+            
+            <section className='answer-section'>
+                {questionBank[currentQuestion].answerChoices.map((item) => {
+              <button onClick={() => correctAnswerHandler(item.isCorrect)}>
+                {item.answerText}
+              </button>
+            })}
+            </section>
+          </>
+          )}
+      </div>
     </div>
   );
 }
-
-export default App;
